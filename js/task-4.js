30d2446324eef2300e8675b49b282836908a053c
ValeriiaSai <kolyadavaleri@gmail.com>
2024-12-09 10:00:24 +0200

    Update task-4.js
    corrections with opetator break

--- js/task-4.js
+++ js/task-4.js
@@ -3,19 +3,22 @@ function getShippingCost(country) {
   switch (country) {
     case 'China':
       price = 100;
+      break;
     case 'Chile':
       price = 250;
+      break;
     case 'Australia':
       price = 170;
+      break;
     case 'Jamaica':
-      price = 120
-      return `Shipping to ${country} will cost ${price} credits`;
+      price = 120;
       break;
     default:
       return `Sorry, there is no delivery to your country`;
   }
+  return `Shipping to ${country} will cost ${price} credits`;
 }
-
+      
 console.log(getShippingCost('China'));
 console.log(getShippingCost('Chile'));
 console.log(getShippingCost('Australia'));
